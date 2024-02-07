36bd63b5f6f39e329e2ff51e43bcfc40503257a3
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-06 23:46:35 -0300

    Melhorando os tests

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -7,14 +7,16 @@ from source.shapes import Circle
 
 faker = Faker() 
 
-def test_circle_area():
-    #arrange
-    radio = faker.random_number()
-    circle = Circle(radio)
+class TestCircle:
     
-    #act
-    result = circle.area()
-    
-    #assert
-    assert result == math.pi * radio**2
-    print(f'A area do circulo vale {result}')
+    def test_circle_area(self):
+        #arrange
+        radio = faker.random_number()
+        circle = Circle(radio)
+        
+        #act 
+        result = circle.area()
+        
+        #assert
+        assert result == math.pi * radio**2
+        print(f'A area do circulo vale {result}')
